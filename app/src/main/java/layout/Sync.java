0ced4aaab4c023bbdafb32a37a16e77b3e93c501
igority <igority@gmail.com>
2016-10-28 17:38:02 +0200

    Frontend on Sync fragment changed, added and initialized controls

--- app/src/main/java/layout/Sync.java
+++ app/src/main/java/layout/Sync.java
@@ -9,6 +9,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.trex.racetracker.R;
@@ -42,9 +44,33 @@ public class Sync extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_sync, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+     //   TextView textView = (TextView) rootView.findViewById(R.id.section_label);
      //   textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-        textView.setText("Vamu ke imame kopce za sinhronizacija so mysql bazata, so takvo sto vrti loading pod nego");
+     //   textView.setText("Vamu ke imame kopce za sinhronizacija so mysql bazata, so takvo sto vrti loading pod nego");
+
+        TextView tvStatusTop = (TextView) rootView.findViewById(R.id.tvStatusTop);
+        EditText etUsername = (EditText) rootView.findViewById(R.id.etUsername);
+        EditText etPassword = (EditText) rootView.findViewById(R.id.etPassword);
+        EditText etOperator = (EditText) rootView.findViewById(R.id.etOperator);
+        Button btnSync = (Button) rootView.findViewById(R.id.btnSync);
+        TextView tvStatusBottom = (TextView) rootView.findViewById(R.id.tvStatusBottom);
+
+
+
+        btnSync.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                //Do stuff here:
+                //1. set status variable to Logging...
+                //2. Call a function from BackgroundWorker to login
+                //3. contact mysql db, return boolean from webservice
+                //4. if false print error msg
+                //5. if true: set global variables
+                //6. call oncreate again and modify it to show different things depending on the global variables
+
+
+            }
+        });
+
         return rootView;
 
     }
