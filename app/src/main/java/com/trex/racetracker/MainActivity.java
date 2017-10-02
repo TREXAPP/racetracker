2e1fb65fbf4aad1e318947405932f37c5e5a2015
igority <igority@gmail.com>
2017-10-02 15:37:06 +0200

    v.1.101 (forgot to update the version) First fix after the event: - Removed pull of the entries, only push is left. Apparently pulling every single entry from the database to every device is slowing the devices to a point of not being usable anymore. Further changes will be needed, as this implies other unforseen changes. Some testing will be neccessary. - Added a delete confirmation popup, as apparently (from the event experience) it is fairly easy to delete an entry unintentionally. More changes issues are pending to be fixed/improved, that were discovered during the event. ---- TODO for the future releases (notes from before the event): - Try to fix the visual bug that shows double entries.

--- app/src/main/java/com/trex/racetracker/MainActivity.java
+++ app/src/main/java/com/trex/racetracker/MainActivity.java
@@ -264,7 +264,7 @@ public class MainActivity extends AppCompatActivity {
 
         //App version
         //if (!globals.contains("version"))
-            editor.putString("version","1.0");
+            editor.putString("version","1.101");
 
         //App version
        // if (!globals.contains("AdminPass"))
